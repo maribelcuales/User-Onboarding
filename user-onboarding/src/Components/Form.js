167f52b48100e89415ab62e82b09b517549866c9
maribelcuales <maribelcuales@gmail.com>
2020-06-13 02:28:42 +0800

    Moved up inputChange event handler.

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import * as yup from "yup";
+import axios from "axios";
 
 const formSchema = yup.object().shape({
   name: yup.string().required("name is a required field"),
@@ -59,6 +60,18 @@ function Form() {
       });
   };
 
+  const inputChange = e => {
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? 
+        e.target.checked : e.target.value
+    };
+    validateChange(e);
+    setFormState(newFormData);
+  };
+
   const formSubmit = e => {
     e.preventDefault();
     axios
@@ -79,67 +92,59 @@ function Form() {
       });
   };
 
-  const inputChange = e => {
-    e.persist();
-    const newFormData = {
-      ...formState,
-      [e.target.name]:
-        e.target.type === "checkbox" ? 
-        e.target.checked : e.target.value
-    };
-    validateChange(e);
-    setFormState(newFormData);
-  };
-
   return (
-    <form onSubmit={formSubmit}>
-      <label htmlFor="name">
-        Name
-        <input
-          id="name"
-          type="text"
-          name="name"
-          value={formState.name}
-          onChange={inputChange}
-        />
-        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
-      </label>
-      <label htmlFor="email">
-        Email
-        <input
-          id="email"
-          type="text"
-          name="email"
-          value={formState.email}
-          onChange={inputChange}
-        />
-        {errors.email.length > 0 ? (<p className="error">{errors.email}</p>) : null}
-      </label>
-      <label htmlFor="password">
-        Password
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={formState.password}
-          onChange={inputChange}
-        />
-        {errors.password.length > 6 ? (<p className="error">{errors.password}</p>) : null}
-      </label>
-      <label htmlFor="terms" className="terms">
-        <input
-          type="checkbox"
-          name="terms"
-          checked={formState.terms}
-          onChange={inputChange}
-        />
-        Terms of Service
-      </label>
-      <button disabled={buttonDisabled}>
-        Submit
-      </button>
-    </form>
-    
+    <div>
+      <form onSubmit={formSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={inputChange}
+          />
+          {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={formState.email}
+            onChange={inputChange}
+          />
+          {errors.email.length > 0 ? (<p className="error">{errors.email}</p>) : null}
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formState.password}
+            onChange={inputChange}
+          />
+          {errors.password.length > 6 ? (<p className="error">{errors.password}</p>) : null}
+        </label>
+        <label htmlFor="terms" className="terms">
+          <input
+            type="checkbox"
+            name="terms"
+            checked={formState.terms}
+            onChange={inputChange}
+          />
+          Terms of Service
+        </label>
+        <button disabled={buttonDisabled}>
+          Submit
+        </button>
+      </form>
+
+      <h3>Users</h3>
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+    </div>  
   )
 };
 
